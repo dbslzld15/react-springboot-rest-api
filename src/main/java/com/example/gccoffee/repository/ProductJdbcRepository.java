83dc4313e75ae60371ef836b1b25bcab26a458bd
dbslzld15 <dbslzld15@naver.com>
2022-04-26 15:30:50 +0900

    feat: 상품 레포지토리 생성

---
+++ src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -0,0 +1,77 @@
+package com.example.gccoffee.repository;
+
+import com.example.gccoffee.model.Category;
+import com.example.gccoffee.model.Product;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.example.gccoffee.utl.Utils.*;
+
+public class ProductJdbcRepository implements ProductRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public ProductJdbcRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return jdbcTemplate.query("select * from products", productRowMapper);
+    }
+
+    @Override
+    public Product insert(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product update(Product product) {
+        return null;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> findByName(String productName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Product> findByCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    private static final RowMapper<Product> productRowMapper = (rs, i) -> {
+        UUID productId = toUUID(rs.getBytes("product_id"));
+        String productName = rs.getString("product_name");
+        Category category = Category.valueOf(rs.getString("category"));
+        long price = rs.getLong("price");
+        String description = rs.getString("description");
+        LocalDateTime createdAt = toLocalDateTime(rs.getTimestamp("created_at"));
+        LocalDateTime updatedAt = toLocalDateTime(rs.getTimestamp("updated_at"));
+
+        return new Product(
+                productId,
+                productName,
+                category,
+                price,
+                description,
+                createdAt,
+                updatedAt
+        );
+    };
+}
